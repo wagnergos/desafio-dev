45a8a6d4ef87bcc84ba2634f505a521f924922bf
Wagner Gonçalves <wagner.wgos@gmail.com>
2021-10-28 23:35:51 -0300

    define seed history to save in database

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  seederStorage: 'sequelize',
   storage: './__tests__/database.sqlite',
   define: {
     timestamps: true,
