bca909ce9b05b01e244ffac9b6261e79106c1103
Wagner Gonçalves <wagner.wgos@gmail.com>
2021-10-30 13:58:58 -0300

    add transaction table style

--- web/src/components/TransactionList/index.js
+++ web/src/components/TransactionList/index.js
@@ -1,11 +1,51 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Table } from './styles';
 
 export default function TransactionList() {
   return (
     <Container>
-      <h1>Transaction</h1>
+      <Table>
+        <thead>
+          <tr>
+            <th>Data da transação</th>
+            <th>Nome da loja</th>
+            <th>Dono da loja</th>
+            <th>Valor</th>
+            <th>Transação</th>
+            <th>Natureza</th>
+            <th>Cartão</th>
+            <th>CPF</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>data</td>
+            <td>nome</td>
+            <td>nome</td>
+            <td>valor</td>
+            <td>tdansação</td>
+            <td>natureza</td>
+            <td>cartão</td>
+            <td>cpf</td>
+          </tr>
+          <tr>
+            <td>data</td>
+            <td>nome</td>
+            <td>nome</td>
+            <td>valor</td>
+            <td>tdansação</td>
+            <td>natureza</td>
+            <td>cartão</td>
+            <td>cpf</td>
+          </tr>
+        </tbody>
+      </Table>
+
+      <div>
+        <button type="button">Anterior</button>
+        <button type="button">Próxima</button>
+      </div>
     </Container>
   );
 }
--- web/src/components/TransactionList/styles.js
+++ web/src/components/TransactionList/styles.js
@@ -5,4 +5,54 @@ export const Container = styled.div`
   grid-area: TL;
 
   background: var(--secondary);
+
+  div {
+    display: flex;
+    flex: 1;
+    margin: 10px 5%;
+    justify-content: flex-end;
+
+    button {
+      padding: 10px;
+      border-radius: 4px;
+
+      & + button {
+        margin-left: 20px;
+      }
+
+      &:hover {
+        background: var(--primary);
+      }
+    }
+  }
+`;
+
+export const Table = styled.table`
+  width: 95%;
+  margin: 20px auto;
+  border-collapse: collapse;
+
+  th,
+  td {
+    padding: 12px;
+  }
+
+  thead th {
+    text-align: left;
+    font-weight: bold;
+    color: var(--white);
+  }
+
+  tbody tr {
+    color: var(--white);
+
+    &:nth-of-type(odd) {
+      color: var(--gray);
+    }
+
+    &:hover {
+      color: var(--white);
+      background: var(--gray);
+    }
+  }
 `;
