22051c45075cd6843be916644e1a4cd4e6de003f
Wagner Gonçalves <wagner.wgos@gmail.com>
2021-10-30 11:02:27 -0300

    create store list component

---
+++ web/src/components/StoreList/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+// SL - Store List
+export const Container = styled.div`
+  grid-area: SL;
+
+  max-height: 100vh;
+  padding: 20px 0;
+  background: var(--primary);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
