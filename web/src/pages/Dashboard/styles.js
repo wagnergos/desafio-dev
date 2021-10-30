1cffbd571271a0f0074af528d929bdf09ccc5f02
Wagner Gonçalves <wagner.wgos@gmail.com>
2021-10-30 15:07:19 -0300

    change grid template rows

--- web/src/pages/Dashboard/styles.js
+++ web/src/pages/Dashboard/styles.js
@@ -8,7 +8,7 @@ export const GridContainer = styled.div`
   display: grid;
 
   grid-template-columns: 1fr 3fr;
-  grid-template-rows: 60px auto;
+  grid-template-rows: 86px auto;
 
   grid-template-areas:
     'SL H'
